4fe6e62946de884224b9f9a11ddce454c9410f8b
yagomateos <47737893+yagomateos@users.noreply.github.com>
2025-03-05 12:11:09 +0100

    Spoty fix

--- app/page.tsx
+++ app/page.tsx
@@ -348,7 +348,7 @@ export default function Home() {
              activePlaylist === 'remixes' ? 'Remixes' : 
              activePlaylist === 'technoRave' ? 'Techno Rave' : 
              activePlaylist === 'techoHouse' ? 'Techo House' : 
-             'Spoti Tech'}
+             'Spoty Tech'}
           </h1>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
